6f931ab0ce5a5cd01012ea29364e31bf174f7e15
RyanBaxterSoftware <ryan.baxter.software@gmail.com>
2021-11-30 17:02:34 -0600

    Add files via upload
    Initial upload of menu that shows all files in the daily format. Need to change format.

---
+++ AdventOfCode2021/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Welcome to Advent of Code 2021")
+	fmt.Println("Select from the following days challenges:")
+	displayMenu()
+	fmt.Println("Make your selection:")
+	text, _ := reader.ReadString('\n')
+	text = strings.Replace(text, "\r\n", "", -1)
+	fmt.Println()
+	processText(text)
+}
+
+func displayMenu() {
+	fmt.Println()
+	path, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	dayFiles := make([]fs.FileInfo, 0)
+	for _, file := range files {
+		if strings.Contains(file.Name(), "aoc2022day") {
+			dayFiles = append(dayFiles, file)
+		}
+	}
+	for num, file := range dayFiles {
+		fmt.Println(num, file.Name())
+	}
+	fmt.Println()
+}
+
+func processText(text string) {
+	switch text {
+	case "0":
+		helloWorld()
+	}
+}
