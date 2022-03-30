db398da05f3830b76a47d6bef70bfe1b8b1868d6
RyanBaxterSoftware <ryan.baxter.software@gmail.com>
2022-03-30 15:35:48 -0500

    Day 12 part 2
    Added testing option to check process and added option to loop to a small cave twice, once.

--- AdventOfCode2021/aoc2021day12.go
+++ AdventOfCode2021/aoc2021day12.go
@@ -21,7 +21,7 @@ func CavePathNavigation() {
 	case "1":
 		navigatePathSimple(inputData)
 	case "2":
-		findUnifiedFlash(inputData)
+		navigatePathAdjusted(inputData)
 	}
 }
 
@@ -54,6 +54,52 @@ func getAllOptions(currentPath []string, options map[string][]string) [][]string
 		tempPath := append(currentPath, option)
 		if option != "end" && (!contains(currentPath, option) || unicode.IsUpper(rune(option[0]))) {
 			allOptions = append(allOptions, getAllOptions(tempPath, options)...)
+		} else if option == "end" {
+			newTempPath := make([]string, 0)
+			// this is done to work around the issue with golang holding slice values by reference instead of by value.
+			for _, cave := range tempPath {
+				newTempPath = append(newTempPath, cave)
+			}
+			allOptions = append(allOptions, newTempPath)
+		}
+	}
+
+	return allOptions
+}
+
+func navigatePathAdjusted(inputData string) {
+	exits := make(map[string][]string)
+	for _, line := range strings.Split(inputData, "\n") {
+		connection := strings.Split(line, "-")
+		if !contains(exits[connection[0]], connection[1]) {
+			exits[connection[0]] = append(exits[connection[0]], connection[1])
+		}
+		if !contains(exits[connection[1]], connection[0]) {
+			exits[connection[1]] = append(exits[connection[1]], connection[0])
+		}
+	}
+	startPath := make([]string, 0)
+	startPath = append(startPath, "start")
+	fmt.Printf("The options are %v\n", exits)
+	allEndOptions := getAllOptionsWithSmallCaveAllowance(startPath, exits, false)
+	fmt.Printf("The total number of options we found is %d\n", len(allEndOptions))
+}
+
+func getAllOptionsWithSmallCaveAllowance(currentPath []string, options map[string][]string, smallCaveRecurred bool) [][]string {
+	// for each next step option
+	// add option to path
+	// get all the possible paths from there
+	// return the end
+
+	allOptions := make([][]string, 0)
+	for _, option := range options[currentPath[len(currentPath)-1]] {
+		tempPath := append(currentPath, option)
+		if option != "end" && option != "start" && (unicode.IsUpper(rune(option[0])) || (!(contains(currentPath, option) && smallCaveRecurred))) {
+			futureSmallCaveRecurred := smallCaveRecurred
+			if contains(currentPath, option) && unicode.IsLower(rune(option[0])) {
+				futureSmallCaveRecurred = true
+			}
+			allOptions = append(allOptions, getAllOptionsWithSmallCaveAllowance(tempPath, options, futureSmallCaveRecurred)...)
 		} else if option == "end" {
 			newTempPath := make([]string, 0)
 			// this is done to work around the issue with golang holding slice values by reference instead of by value.
